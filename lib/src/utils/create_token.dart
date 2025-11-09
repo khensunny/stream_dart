98d88ff8858004f959a0ac5c1902746ec5dccca8
M.K. Jele <mphokjele@gmail.com>
2025-11-09 21:42:30 +0200

    fix(token): use base64Url encoded secret for JWT recipient

--- lib/src/utils/create_token.dart
+++ lib/src/utils/create_token.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+
 import 'package:jose_plus/jose.dart';
 
 String createUserJwtToken(String secret, Map<String, dynamic> payload) {
   final claims = JsonWebTokenClaims.fromJson(payload);
 
+  final secretBytes = utf8.encode(secret);
+  final base64UrlSecret = base64Url.encode(secretBytes);
+
   final builder = JsonWebSignatureBuilder()
     ..content = claims.toJson()
     ..addRecipient(
-      JsonWebKey.fromJson({'kty': 'oct', 'k': secret}),
+      JsonWebKey.fromJson({'kty': 'oct', 'k': base64UrlSecret}),
       algorithm: 'HS256',
     );
 
